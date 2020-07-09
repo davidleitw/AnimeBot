7af74e543f618e5b09b3e6ab65bcfbec4a55c483
davidlei <davidleitw@gmail.com>
2020-07-10 03:07:34 +0800

    Test line bot api

--- api/handleEventTypeFollow.go
+++ api/handleEventTypeFollow.go
@@ -1,20 +1,30 @@
 package api
 
-import "github.com/line/line-bot-sdk-go/linebot"
+import (
+	"log"
+
+	"github.com/line/line-bot-sdk-go/linebot"
+)
 
 func HandleEventTypeFollow(event *linebot.Event, bot *linebot.Client) {
 	switch message := event.Message.(type) {
 	case *linebot.TextMessage:
 		if message.Text == "!help" {
-			bot.ReplyMessage(
+			_, err := bot.ReplyMessage(
 				event.ReplyToken,
 				linebot.NewTextMessage("Help message: Help!"),
-			)
+			).Do()
+			if err != nil {
+				log.Println("!help message error = ", err)
+			}
 		} else {
-			bot.ReplyMessage(
+			_, err := bot.ReplyMessage(
 				event.ReplyToken,
 				linebot.NewTextMessage("Anime Bot: "+message.Text),
-			)
+			).Do()
+			if err != nil {
+				log.Println("Normal text message error = ", err)
+			}
 		}
 	}
 }

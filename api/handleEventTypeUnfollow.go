454e90445a96282825697082e113800656e142c1
davidlei <davidleitw@gmail.com>
2020-07-15 01:49:53 +0800

    unfollow event

--- api/handleEventTypeUnfollow.go
+++ api/handleEventTypeUnfollow.go
@@ -1,7 +1,12 @@
 package api
 
-import "github.com/line/line-bot-sdk-go/linebot"
+import (
+	"github.com/davidleitw/AnimeBot/model"
+	"github.com/line/line-bot-sdk-go/linebot"
+)
 
 func HandleEventTypeUnfollow(event *linebot.Event, bot *linebot.Client) {
-
+	// 封鎖line bot的時候刪除該用戶資料庫的資料
+	userID := event.Source.UserID
+	model.DB.Where("user_id = ?", userID).Delete(model.User{})
 }

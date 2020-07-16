297299c23c29493dfe122d4c3b7494c89f52d0d0
davidlei <davidleitw@gmail.com>
2020-07-16 23:04:54 +0800

    先把unfollow功能關掉

--- api/handleEventTypeUnfollow.go
+++ api/handleEventTypeUnfollow.go
@@ -1,12 +1,12 @@
 package api
 
 import (
-	"github.com/davidleitw/AnimeBot/model"
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func HandleEventTypeUnfollow(event *linebot.Event, bot *linebot.Client) {
 	// 封鎖line bot的時候刪除該用戶資料庫的資料
-	userID := event.Source.UserID
-	model.DB.Where("user_id = ?", userID).Delete(model.User{})
+	// userID := event.Source.UserID
+	// model.DB.Where("user_id = ?", userID).Delete(model.User{})
+	// 為了收集資料 所以暫時不要開啟這個功能
 }
